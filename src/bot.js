7c10df1c3220007504ccc8774c8f41add14bb0f4
Huy Sam <huyprolove114@gmail.com>
2022-02-16 12:08:36 -0500

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,6 @@ require('dotenv').config(); //initialize dotenv
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const axios = require('axios');
-const keepAlive = require("./server")
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -31,6 +30,5 @@ client.on('message', async msg => {
 }
 
 
-keepAlive()
 //make sure this line is the last line
 client.login(process.env.TOKEN)
